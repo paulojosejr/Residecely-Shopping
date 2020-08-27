080ba69660353ed1cbd2517a630e89534a0b8d2f
Paulo Araújo <paulojosejr95@gmail.com>
2020-08-27 08:56:47 -1100

    flappy bird progress count

--- flappyBird/flappy.js
+++ flappyBird/flappy.js
@@ -70,9 +70,74 @@ function Barriers(height, width, opening, space, notifyPoint){
     }
 }
 
-const barriers = new Barriers(700, 1100, 200, 400)
-const gameArea = document.querySelector('[wm-flappy]')
-barriers.pairs.forEach(pair => gameArea.appendChild(pair.element))
-setInterval(() => {
-    barriers.animate()
-}, 20);
+function Bird(heightGame){
+    let flying = false
+
+    this.element = newElement('img', 'bird')
+    this.element.src = 'imgs/bird.png'
+
+    this.getY = () => parseInt(this.element.style.bottom.split('px')[0])
+    this.setY = y => this.element.style.bottom = `${y}px`
+
+    window.onkeydown = e => flying = true
+    window.onkeyup = e => flying = false
+
+    this.animate = () => {
+        const newY = this.getY() + (flying ? 8 : -5)
+        const maximumHeight = heightGame - this.element.clientHeight
+
+        if(newY <= 0){
+            this.setY(0)
+        }else if (newY >= maximumHeight){
+            this.setY(maximumHeight)
+        }else{
+            this.setY(newY)
+        }
+    }
+    this.setY(heightGame / 2)
+}
+
+
+function Progress() {
+    this.element = newElement('span', 'progress')
+    this.updatePoints = points => {
+        this.element.innerHTML = points
+    }
+    this.updatePoints(0)
+}
+// const bird = new Bird(700)
+// const gameArea = document.querySelector('[wm-flappy]')
+// const barriers = new Barriers(700, 1200, 200, 400)
+// gameArea.appendChild(bird.element)
+// gameArea.appendChild(new Progress().element)
+// barriers.pairs.forEach(pair => gameArea.appendChild(pair.element))
+// setInterval(() => {
+//     barriers.animate()
+//     bird.animate()
+// }, 20);
+
+function FlappyBird(){
+    let points = 0
+
+    const gameArea = document.querySelector('[wm-flappy]')
+    const height = gameArea.clientHeight
+    const width = gameArea.clientWidth
+
+    const progress = new Progress()
+    const barriers = new Barriers(height, width, 200, 400, () => progress.updatePoints(++points))
+    const bird = new Bird(height)
+
+    gameArea.appendChild(progress.element)
+    gameArea.appendChild(bird.element)
+    barriers.pairs.forEach(pair => gameArea.appendChild(pair.element))
+    
+    this.start = () => {
+        //game loop
+        const timer = setInterval(() => {
+          barriers.animate()
+          bird.animate()  
+        },20)
+    }
+}
+
+new FlappyBird().start()
